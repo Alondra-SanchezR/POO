4352f80d50090771ff50217e597f6ea36d72ed09
Alondra Sánchez <alondra_3141240257@utd.edu.mx>
2025-12-01 19:12:28 -0600

    commit_01_02_25

---
+++ P3/coches_system/AVANCES.py
@@ -0,0 +1,18 @@
+"""
+1ER DICIEMBRE
+    1) Implemetación de MVC
+    2) POO
+    3) INTERFACES:
+        3.1 menu_principal()
+        3.2 menu_acciones()
+        3.3 insertar_autos()
+        3.4 consultar_autos()
+        3.5 cambiar_autos()
+        3.6 borrar_autos()
+
+ Productos Entregables:
+    **Estructura del proyecto basado en MVC.
+    **Modulo principal "main".
+    **Interacción con las interfaces
+    **Nombre del Commit: "commit_01_02_25".
+"""
---
+++ P3/coches_system/main.py
@@ -0,0 +1,10 @@
+from tkinter import *
+from view import interfaz
+class App():
+    def __init__(self,ventana):
+        interfaz.InterfacesMenu(ventana)
+
+if __name__=="__main__":
+    ventana=Tk()
+    app=App(ventana)
+    ventana.mainloop()
